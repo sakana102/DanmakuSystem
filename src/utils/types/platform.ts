8405b6e7a2793e3bc31dd7b42a940869c965bfcf
sakana102 <main.sakana@gmail.com>
2026-03-11 15:23:31 +0900

    feat: createPlatformObject を定義

--- src/utils/types/platform.ts
+++ src/utils/types/platform.ts
@@ -2,3 +2,15 @@ import { z } from "zod";
 
 export const PlatformSchema = z.enum(["youtube", "twitch", "kick", "openrec", "twicas"]);
 export type Platform = z.infer<typeof PlatformSchema>;
+
+export const createPlatformObject = <T extends z.ZodTypeAny>(itemSchema: T) => {
+  const shape = PlatformSchema.options.reduce(
+    (acc, platform) => {
+      acc[platform] = itemSchema;
+      return acc;
+    },
+    {} as Record<Platform, T>,
+  );
+
+  return z.object(shape);
+};
