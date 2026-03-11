b42ba8b7af1e7be6cc6630fb3eddea1bac0275a1
sakana102 <main.sakana@gmail.com>
2026-03-11 15:12:07 +0900

    feat: CanvasManager を実装

---
+++ src/entrypoints/content/core/canvas-manager.ts
@@ -0,0 +1,65 @@
+export class CanvasManager {
+  private canvas: HTMLElement;
+
+  constructor(canvas: HTMLElement) {
+    this.canvas = canvas;
+  }
+
+  public update(canvas: HTMLElement) {
+    this.canvas = canvas;
+  }
+
+  public append(element: HTMLElement): void {
+    this.canvas.append(element);
+  }
+
+  public clear(): void {
+    this.cancelAllAnimations();
+    this.replaceChildren();
+  }
+
+  public getElement(): HTMLElement {
+    return this.canvas;
+  }
+
+  public getWidth(): number {
+    return this.canvas.clientWidth;
+  }
+
+  public getHeight(): number {
+    return this.canvas.clientHeight;
+  }
+
+  public measureItemHeight(): number {
+    const div = document.createElement("div");
+    div.textContent = "DanmakuSystem";
+    div.className = "DanmakuSystem__check";
+    this.canvas.append(div);
+    const result = div.clientHeight;
+    div.remove();
+    return result;
+  }
+
+  public measureItemWidth(element: HTMLElement): number {
+    const clone = element.cloneNode(true) as HTMLElement;
+    clone.className = "DanmakuSystem__check";
+    this.canvas.append(clone);
+    const result = clone.clientWidth;
+    clone.remove();
+    return result;
+  }
+
+  public getItemPositionX(element: HTMLElement): number {
+    const canvasRect = this.canvas.getBoundingClientRect();
+    const itemRect = element.getBoundingClientRect();
+    return itemRect.left - canvasRect.left;
+  }
+
+  public replaceChildren(): void {
+    this.canvas.replaceChildren();
+  }
+
+  public cancelAllAnimations(): void {
+    this.canvas.getAnimations().forEach((animation) => animation.cancel());
+  }
+}
