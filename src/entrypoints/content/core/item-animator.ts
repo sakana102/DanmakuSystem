cdc114583e4ffffbdd9d3acba19b7db9b64227ab
sakana102 <main.sakana@gmail.com>
2026-03-11 15:12:33 +0900

    feat: ItemAnimator を実装

---
+++ src/entrypoints/content/core/item-animator.ts
@@ -0,0 +1,78 @@
+import { Platform } from "@/utils/types/platform";
+import { Settings } from "@/utils/types/settings";
+import { Item } from "@/entrypoints/content/types/item";
+import { LaneType } from "@/entrypoints/content/types/lane";
+import { CanvasManager } from "@/entrypoints/content/core/canvas-manager";
+
+export class ItemAnimator {
+  private platform: Platform;
+  private canvas: CanvasManager;
+  private settings: Settings;
+
+  constructor(platform: Platform, canvas: CanvasManager, settings: Settings) {
+    this.platform = platform;
+    this.canvas = canvas;
+    this.settings = settings;
+  }
+
+  public update(settings: Settings) {
+    this.settings = settings;
+  }
+
+  public async animate(
+    type: LaneType,
+    item: Item,
+    laneLength: number,
+    laneIndex: number,
+    onFinish: () => void,
+  ): Promise<void> {
+    this.canvas.append(item.element);
+
+    if (type === "flow") {
+      item.element.style.top = `${item.height * laneIndex}px`;
+      item.element.style.left = `${this.canvas.getWidth()}px`;
+      await this.animateFlow(item);
+    }
+
+    if (type === "up") {
+      item.element.style.top = `${item.height * laneIndex}px`;
+      item.element.style.left = `${(this.canvas.getWidth() - item.width) / 2}px`;
+      await this.animateFixed(item);
+    }
+
+    if (type === "down") {
+      item.element.style.top = `${item.height * (laneLength - 1 - laneIndex)}px`;
+      item.element.style.left = `${(this.canvas.getWidth() - item.width) / 2}px`;
+      await this.animateFixed(item);
+    }
+
+    onFinish();
+    item.element.remove();
+  }
+
+  private async animateFlow(item: Item): Promise<void> {
+    console.log(item);
+    const animation = item.element.animate(
+      [
+        { transform: `translate3d(0, 0, 0)` },
+        { transform: `translate3d(${-(this.canvas.getWidth() + item.width)}px, 0, 0)` },
+      ],
+      {
+        duration: this.settings.displayTime[this.platform] * 1000,
+        easing: "linear",
+      },
+    );
+
+    await animation.finished;
+    item.state = "finished";
+  }
+
+  private async animateFixed(item: Item): Promise<void> {
+    const animation = item.element.animate([{ opacity: 1 }, { opacity: 1 }], {
+      duration: this.settings.displayTime[this.platform] * 1000,
+    });
+
+    await animation.finished;
+    item.state = "finished";
+  }
+}
