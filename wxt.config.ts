d879ce2f028d720e2cec1bc988318af2ae4151e1
sakana102 <main.sakana@gmail.com>
2026-03-11 15:24:49 +0900

    chore: manifest を設定

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
+  manifest: {
+    name: "DanmakuSystem",
+    description: "DESCRIPTION",
+    permissions: ["storage", "fontSettings"],
+  },
   modules: ["@wxt-dev/module-react"],
 });
